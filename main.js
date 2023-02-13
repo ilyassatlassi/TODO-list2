fb2de07aa0cd8085ad56f940c3396c158726f2e5
ilyass atlassi <atlassiilyass@gmail.com>
2023-02-13 10:25:17 +0100

    create sript file

---
+++ main.js
@@ -0,0 +1,5 @@
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const add = document.getElementById('add');
+const form = document.getElementById('form');
+console.log(title, author);
